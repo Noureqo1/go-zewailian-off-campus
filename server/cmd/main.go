f543ece325e81e522567a6680d1451eaaaf3e355
noureqo <128331928+noureqo@users.noreply.github.com>
2025-03-08 05:56:25 +0200

    closes #1 User Model

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -3,11 +3,25 @@ package main
 import (
 	"log"
 	"server/db"
+	"server/internal/user"
+	"server/router"
 )
 
-func main() {
-	_, err := db.NewDatabase()
+func main() { 
+	log.Println("Starting the application...")
+	dbConn, err := db.NewDatabase()
 	if err != nil {
-		log.Fatalf("could not initialize database connection: %s", err)
+		log.Fatalf("could not initialize database connection: %v", err)
+	}
+	log.Println("Database connection established")
+
+	userRep := user.NewRepository(dbConn.GetDB())
+	userSvc := user.NewService(userRep)
+	userHandler := user.NewHandler(userSvc)
+
+	router.InitRouter(userHandler)
+	log.Println("Starting server on :8080")
+	if err := router.Start(":8080"); err != nil {
+		log.Fatalf("could not start server: %v", err)
 	}
 }
