9bdf017242e79f1568899894886eb609cf2f1770
noureqo <noureqomohamed14@gamil.com>
2025-04-19 01:17:07 +0200

    implementing Google Oauth0.2. #17

---
+++ server/internal/oauth/oauth.go
@@ -0,0 +1,66 @@
+package oauth
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+var googleOauthConfig *oauth2.Config
+
+// InitGoogleOAuth initializes the Google OAuth2 config from environment variables
+func InitGoogleOAuth() {
+	redirectURL := os.Getenv("GOOGLE_OAUTH_REDIRECT_URL")
+	fmt.Println("DEBUG: GOOGLE_OAUTH_REDIRECT_URL =", redirectURL) // Debug print
+	googleOauthConfig = &oauth2.Config{
+		RedirectURL:  redirectURL,
+		ClientID:     os.Getenv("GOOGLE_OAUTH_CLIENT_ID"),
+		ClientSecret: os.Getenv("GOOGLE_OAUTH_CLIENT_SECRET"),
+		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"},
+		Endpoint:     google.Endpoint,
+	}
+}
+
+// GoogleLoginHandler redirects user to Google's OAuth2 consent page
+func GoogleLoginHandler(w http.ResponseWriter, r *http.Request) {
+	url := googleOauthConfig.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
+// GoogleCallbackHandler handles Google's OAuth2 callback and returns user info
+func GoogleCallbackHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+	code := r.URL.Query().Get("code")
+	if code == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "Code not found")
+		return
+	}
+	token, err := googleOauthConfig.Exchange(ctx, code)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Token exchange error: %v", err)
+		return
+	}
+	client := googleOauthConfig.Client(ctx, token)
+	resp, err := client.Get("https://www.googleapis.com/oauth2/v2/userinfo")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Failed getting user info: %v", err)
+		return
+	}
+	defer resp.Body.Close()
+	var userInfo map[string]interface{}
+	if err := json.NewDecoder(resp.Body).Decode(&userInfo); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Failed decoding user info: %v", err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(userInfo)
+}
